bdd206d3ff936f3d1b05a32d27a3a263822b448a
Abel-Mekonen <abelmekonen559@gmail.com>
2022-01-25 20:59:21 +0300

    intial commit

---
+++ src/main/java/com/delala/delala/DelalaApplication.java
@@ -0,0 +1,13 @@
+package com.delala.delala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delala/delala/DelalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delala.delala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
