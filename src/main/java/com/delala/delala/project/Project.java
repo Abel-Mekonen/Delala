c786cd057e040e0e54a22ce2a199124b49764e2a
hello-world-was-taken <haileformal@gmail.com>
2022-01-28 01:56:08 +0300

    update skills

--- src/main/java/com/delala/delala/project/Project.java
+++ src/main/java/com/delala/delala/project/Project.java
@@ -6,6 +6,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.delala.delala.skill.Skill;
+
 import lombok.Data;
 
 @Data
