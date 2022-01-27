b781389957b1444de52899e000503a9926c1f812
Bruk Tedla <biruk.tedla93@gmail.com>
2022-01-28 00:44:13 +0300

    some pages

--- src/main/java/com/delala/delala/TestController.java
+++ src/main/java/com/delala/delala/TestController.java
@@ -1,7 +1,5 @@
 package com.delala.delala;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
